84ade47325bdad05eed4173bc7aefdea228aabba
Adriano Rocha <adriano.olr@gmail.com>
2018-01-28 09:40:36 -0300

    modelo de personagem

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,12 +8,15 @@ public class Player : NetworkBehaviour {
 	[SerializeField] float speed;
 	[SerializeField] float rotationSpeed;
 	[SerializeField] float gravity;
+	[SerializeField] Animator animator;
 
 	public Interactable interactingObject;
 
 	public List<Item> itemList;
 
 	CharacterController controller;
+
+	private float idleTime = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -31,6 +34,26 @@ public class Player : NetworkBehaviour {
 
 		float horizontal = Input.GetAxis("Horizontal");
 		float vertical = Input.GetAxis("Vertical");
+		float angle = -1;
+
+		float anaogSpeed = (Mathf.Abs(horizontal) + Mathf.Abs(vertical)) * 2;
+
+		animator.SetFloat("speed", anaogSpeed);
+
+		if (horizontal != 0.0f || vertical != 0.0f) {
+     		angle = Mathf.Atan2(vertical, -horizontal) * Mathf.Rad2Deg;
+     		// Do something with the angle here.
+			angle -= 90;
+			Transform child = transform.GetChild(0);
+			child.localEulerAngles = new Vector3(child.localEulerAngles.x, angle, child.localEulerAngles.z);
+ 		} else {
+			idleTime += Time.deltaTime;
+		}
+
+		if (idleTime >= 10) {
+			idleTime = 0;
+			animator.SetTrigger("idle");
+		}
 
 		float camhorizontal = Input.GetAxis("CHorizontal");
 		transform.Rotate(0, camhorizontal * rotationSpeed * Time.deltaTime, 0);
